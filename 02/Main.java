857806ed8b8e519d8dd73f0a00d0968e9736a9cf
Carlos Eduardo Müller de Carvalho <kdu428@gmail.com>
2023-12-02 17:21:10 +0000

    wip(day 2): part two

--- 02/Main.java
+++ 02/Main.java
@@ -3,47 +3,87 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.*;
-public class Main{
-    public static enum Colors{
+
+public class Main {
+    public static enum Colors {
         blue(14),
         red(12),
         green(13);
+
         private int max;
-        private Colors(int max){
+
+        private Colors(int max) {
             this.max = max;
         }
-        boolean isbiggerThanMax(int qtde){
+
+        boolean isbiggerThanMax(int qtde) {
             return qtde > max;
         }
     }
 
-    public static Integer getGameNumber(String line){
+    public static class Color {
+        public int min = Integer.MAX_VALUE;
+
+        public void isBigger(int qtde){
+                min = min > qtde? qtde : min;
+            }
+
+    }
+
+    public static Integer getGameNumber(String line) {
         Pattern pattern = Pattern.compile("Game (\\d+):");
         Matcher matcher = pattern.matcher(line);
         matcher.find();
         return Integer.parseInt(matcher.group(1));
     }
 
-    public static boolean isPossible(String line, boolean part1){
+    public static Matcher getMatches(String line) {
         Pattern pattern = Pattern.compile("((\\d+) (blue|red|green))");
         Matcher matcher = pattern.matcher(line);
+        return matcher;
+    }
+
+    public static boolean isPossible(String line, boolean part1) {
+        Matcher matcher = getMatches(line);
         while (matcher.find()) {
-            if(Colors.valueOf(matcher.group(3)).isbiggerThanMax(Integer.parseInt(matcher.group(2)))){
+            if (Colors.valueOf(matcher.group(3)).isbiggerThanMax(Integer.parseInt(matcher.group(2)))) {
                 return false;
             }
         }
         return true;
     }
 
+    public static int getMinValuesMultiplied(String line, boolean part1){
+        var blue = new Color();
+        var green = new Color();
+        var red = new Color();
+        Matcher matcher = getMatches(line);
+        while (matcher.find()) {
+            switch(Colors.valueOf(matcher.group(3))){
+                case Colors.blue -> blue.isBigger(Integer.parseInt(matcher.group(2)));
+                case Colors.red ->  red.isBigger(Integer.parseInt(matcher.group(2)));
+                case Colors.green ->  green.isBigger(Integer.parseInt(matcher.group(2)));
+            }
+        }
+        System.out.println(line + " " + blue.min + "blue "+  red.min +"red " + green.min + "green " + blue.min * red.min * green.min);
+        return blue.min * red.min * green.min;
+    }
+
     public static void main(String[] args) throws IOException {
-        var part1 = true;
-        String path = part1 ? "input-sample.txt" : "input-part2.txt";
+        var part1 = false;
+        String path = "input-sample.txt" ;
         var sum = Files.readAllLines(Paths.get(path)).stream()
                 .map(line -> {
-                   Integer gameNumber =  getGameNumber(line);
-                   boolean possible = isPossible(line,part1);
-                   System.out.println(line +" " + possible);
-                   return possible? gameNumber : 0;
+                    Integer gameNumber = getGameNumber(line);
+                    if (part1) {
+                        boolean possible = isPossible(line, part1);
+                        System.out.println(line + " " + possible);
+                        return possible ? gameNumber : 0;
+                    }
+                    int cube = getMinValuesMultiplied(line, part1);
+                   
+                    return cube;
+
                 })
                 .mapToInt(line -> line)
                 .sum();
