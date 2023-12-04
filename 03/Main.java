0985b2f581328dd3452d5598bcc7d01bb08760e2
carlosmuller <kdu428@gmail.com>
2023-12-04 15:47:19 -0300

    wip(parte 3): pegando qual caracter que deu match

--- 03/Main.java
+++ 03/Main.java
@@ -3,17 +3,38 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.*;
+
 import static java.util.stream.Collectors.*;
 
 public class Main {
 
-    record Number(int line, int start, int end, int number) {
+    record Number(int line, int start, int end, int number, char c) {
     }
+
     static ArrayList<Number> numbers = new ArrayList<>();
     static Map<Integer, List<Number>> numberByLine = new HashMap<Integer, List<Number>>();
-    public static long parte1(char[][] engine, List<String> allLines){
+
+    public static char getNotDotChar(String string) {
+        char first = '.';
+        char last = '.';
+        boolean f = false;
+        for (char c : string.toCharArray()) {
+            if (c != '.') {
+                last = c;
+                if (c == '*') {
+                   return '*';
+                }
+                if(!f) {
+                    first = c;
+                }
+            }
+        }
+        return first != last ? first : last;
+    }
+
+    public static long parte1(char[][] engine, List<String> allLines) {
         var sum = 0L;
-        
+
         for (int i = 0; i < engine.length; i++) {
             String line = allLines.get(i);
             Pattern pattern = Pattern.compile("\\d+");
@@ -24,13 +45,13 @@ public class Main {
                 int start = matcher.start();
                 int end = matcher.end();
                 // System.out.println("Linha %d: number %d start %d end %d".formatted(i, number, start, end));
-                
+
                 // char anterior
                 if (start > 0 && engine[i][start - 1] != '.') {
                     // System.out.println(
                     //         "Linha %d: number: %d  char anterior: %s".formatted(i, number, engine[i][start - 1]));
                     sum += number;
-                    numbers.add(new Number(i, start, end, number));
+                    numbers.add(new Number(i, start, end, number, engine[i][start - 1]));
                     continue;
                 }
                 // mesma linha próximo char
@@ -38,7 +59,7 @@ public class Main {
                     // System.out.println("Linha %d: number: %d  %d char posterior: %s".formatted(i, number,
                     //         end + 1, engine[i][end + 1]));
                     sum += number;
-                    numbers.add(new Number(i, start, end, number));
+                    numbers.add(new Number(i, start, end, number, line.charAt(end)));
                     continue;
                 }
                 // sequencia embaixo do atual com diagonais
@@ -50,7 +71,8 @@ public class Main {
                         // System.out
                         //         .println("Linha %d: number: %d subtring da abaixo: %s".formatted(i, number, substring));
                         sum += number;
-                        numbers.add(new Number(i, start, end, number));
+
+                        numbers.add(new Number(i, start, end, number,getNotDotChar(substring)));
                         continue;
                     }
                 }
@@ -63,7 +85,7 @@ public class Main {
                         // System.out
                         //         .println("Linha %d: number: %d subtring da acima: %s".formatted(i, number, substring));
                         sum += number;
-                        numbers.add(new Number(i, start, end, number));
+                        numbers.add(new Number(i, start, end, number, getNotDotChar(substring)));
                         continue;
                     }
                 }
@@ -72,7 +94,7 @@ public class Main {
         return sum;
     }
 
-    public static long parte2(char[][] engine, List<String> allLines){
+    public static long parte2(char[][] engine, List<String> allLines) {
         var sum = 0L;
         for (int i = 0; i < engine.length; i++) {
             String line = allLines.get(i);
@@ -84,21 +106,22 @@ public class Main {
                 int end = matcher.end();
                 var numbersInSameLine = numberByLine.get(i);
                 //checando na mesma linha
-                if(numbersInSameLine!= null){
-                    var possibleEngineParts = numbersInSameLine.stream().filter(n -> n.start == start-1 || n.end == start-1).toList();
-                    numbers.addAll(possibleEngineParts);	
+                if (numbersInSameLine != null) {
+                    var possibleEngineParts = numbersInSameLine.stream().filter(n -> n.start == start - 1 || n.end == start - 1).toList();
+                    numbers.addAll(possibleEngineParts);
                 }
-                var nextLineNumbers = numberByLine.get(i+1);
-                var beforeLines = numberByLine.get(i-1);
-                
-                if(numbers.size() == 2){
+                var nextLineNumbers = numberByLine.get(i + 1);
+                var beforeLines = numberByLine.get(i - 1);
+
+                if (numbers.size() == 2) {
                     System.out.println("line %d: start %s %s".formatted(i, start, numbers));
-                    sum = sum + ( numbers.get(0).number * numbers.get(0).number );
+                    sum = sum + (numbers.get(0).number * numbers.get(0).number);
                 }
             }
         }
         return sum;
     }
+
     public static void main(String[] args) throws IOException {
         String path = "input-sample.txt";
         List<String> allLines = Files.readAllLines(Paths.get(path));
